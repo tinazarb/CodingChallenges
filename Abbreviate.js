b63059af454fb9cad980fa1a4441900b2c26d473
Tina Zarbaliev <80288782+tinazarb@users.noreply.github.com>
2021-04-26 20:53:09 -0400

    Add files via upload

---
+++ Abbreviate.js
@@ -0,0 +1,27 @@
+// Challenge:
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+Patrick Feeney => P.F
+
+// My Solution:
+function abbrevName(name){
+  const abbrev = name.split(' ');
+  return abbrev[0].charAt(0).toUpperCase() + '.' + abbrev[1].charAt(0).toUpperCase()
+}
+
+// Top Solutions:
+function abbrevName(name){
+
+  var nameArray = name.split(" ");
+  return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+}
+
+function abbrevName(name){
+  return name.split(' ').map(x => x.substr(0, 1).toUpperCase()).join('.');
+}
