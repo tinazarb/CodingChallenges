d9b0c49ca73bdb6d5b26d0e51bf713539f7694ce
tinazarb <tinazarb@gmail.com>
2021-07-13 16:23:07 -0400

    new challenge

---
+++ 7kyu/Algorithms_SumOfArray.js
@@ -0,0 +1,52 @@
+// Challenge
+Sum of array singles
+
+In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+
+More examples in the test cases.
+
+Good luck!
+
+If you like this Kata, please try:
+
+Sum of prime-indexed elements
+
+Sum of integer combinations
+
+
+// My Solution:
+function repeats(arr){
+    return arr.filter(n => arr.indexOf(n) === arr.lastIndexOf(n)).reduce((a,c) => a+c, 0)
+}
+
+// Other Solutions:
+function repeats(arr){
+  return arr.filter(v => arr.indexOf(v) === arr.lastIndexOf(v)).reduce((a,b) => a + b, 0);
+};
+
+function repeats(arr) {
+  let seen = new Set();
+  let sum = 0;
+  
+  for (let v of arr) {
+    if (!seen.has(v)) {
+      seen.add(v);
+      sum += v;
+    } else {
+      sum -= v;
+    }
+  }
+  
+  return sum;
+};
+
+function repeats(arr){
+   return arr
+   .filter(x => arr.indexOf(x) === arr.lastIndexOf(x))
+   .reduce((a, b) => a + b);
+};
+
+
+#Fundamentals
