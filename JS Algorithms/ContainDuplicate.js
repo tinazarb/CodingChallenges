4dd1d4b1d92453e700b6d345bacf4abcb1addabe
tinazarb <tinazarb@gmail.com>
2021-08-24 21:38:52 -0400

    new challenge

---
+++ JS Algorithms/ContainDuplicate.js
@@ -0,0 +1,28 @@
+// Challenge:
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+// My Solution:
+function containDup(arr){
+   let map = {}
+   for(let i = 0; i < arr.length; i++){
+     map[arr[i]] = map[arr[i]] + 1 || 1
+
+     if(map[arr[i]] > 1){
+       return true
+     }
+   }
+   return false
+ }
