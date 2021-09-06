4ff85f32e3b73659b3119e6f742ebd418187b0fa
tinazarb <tinazarb@gmail.com>
2021-09-06 17:37:30 -0400

    new challenge

---
+++ 8kyu/Fundamentals_MakeUpperCase.js
@@ -0,0 +1,12 @@
+// Challenge
+Make Upper Case
+
+Write a function which converts the input string to uppercase.
+
+// My Solution:
+function makeUpperCase(str) {
+  return str.toUpperCase()
+}
+
+// Other Solutions:
+const makeUpperCase = str => str.toUpperCase();
