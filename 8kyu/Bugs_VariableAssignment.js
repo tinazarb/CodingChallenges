2cfa6b7f99d14c116d57f42b6400713caf46fe34
tinazarb <tinazarb@gmail.com>
2021-09-17 11:00:43 -0400

    new challenge

---
+++ 8kyu/Bugs_VariableAssignment.js
@@ -0,0 +1,15 @@
+// Challenge:
+Basic variable assignment
+
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+// My Solution:
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+
+
+// Other Solutions:
+let name = 'codewa.rs';
+
+var name = 'codewa.rs';
