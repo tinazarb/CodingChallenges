c28cd8faaaf74c521093f898ba33bdbbde7e4803
tinazarb <tinazarb@gmail.com>
2021-09-22 19:52:51 -0400

    new challenge

---
+++ 8kyu/Fundamentals_NameonBillboard.js
@@ -0,0 +1,32 @@
+// Challenge
+Name on billboard
+
+You can print your name on a billboard ad. Find out how much it will cost you. Each letter has a default price of £30, but that can be different if you are given 2 parameters instead of 1.
+
+You can not use multiplier "*" operator.
+
+If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a letter).
+
+console.log((billboard("Jeong-Ho Aristotelis"), 600))
+
+// My Solution:
+function billboard(name, price = 30){
+  let totalPrice = 0
+  for (let i = 0; i < name.length; i++) {
+    totalPrice += price 
+  }
+  return totalPrice
+} 
+
+// Other Solutions:
+function billboard(name, price = 30){
+  return name.repeat(price).length;
+} 
+
+function billboard(name, price = 30){
+  return name.length/(1/price)
+} 
+
+function billboard(name, price = 30) {
+  return name.split('').reduce((sum, letter) => sum + price, 0); 
+}
