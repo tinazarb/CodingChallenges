83a3dbe901649f232af600464c895c1c7a04fed0
tinazarb <tinazarb@gmail.com>
2021-10-11 20:43:54 -0400

    new challenge

---
+++ 7kyu/Fundamentals_FlattenandSort.js
@@ -0,0 +1,27 @@
+// Challenge
+Flatten and sort an array
+
+Challenge:
+
+Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+Example:
+
+Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+Addendum:
+
+Please, keep in mind, that JavaScript is by default sorting objects alphabetically. For more information, please consult:
+
+http://stackoverflow.com/questions/6093874/why-doesnt-the-sort-function-of-javascript-work-well
+
+  Test.assertSimilar(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  Test.assertSimilar(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]), [1, 2, 3, 4, 5, 6, 100]);
+
+// My Solution:
+function flattenAndSort(array) {
+  return [].concat(...array).sort((a,b) => (a-b))
+}
+
+// Other Solutions:
+const flattenAndSort = array => [].concat(...array).sort((a,b)=>a-b)
