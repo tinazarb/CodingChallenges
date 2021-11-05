9c07ba298a2b4a2e49be2fbd0f03365c0ab5a303
tinazarb <tinazarb@gmail.com>
2021-11-04 21:22:22 -0400

    new challenge

---
+++ 8kyu/Fundamentals_SumTheString.js
@@ -0,0 +1,38 @@
+// Challenge
+Sum The Srings
+
+Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)
+
+"4",  "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2"
+
+Notes:
+
+    If either input is an empty string, consider it as zero.
+
+    Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+
+// My Solution:
+function sumStr(a,b) {
+ return (Number(a) + Number (b)).toString()
+}
+
+// Other Solutions:
+function sumStr(a,b) {
+  return String(Number(a)+Number(b));
+}
+
+function sumStr(a,b) {
+  return (+a+ +b)+''  
+}
+
+function sumStr(a, b) {
+  return ((parseInt(a) || 0) + (parseInt(b) || 0)).toString();
+}
+
+const sumStr = (a, b) => String(+a + +b)
