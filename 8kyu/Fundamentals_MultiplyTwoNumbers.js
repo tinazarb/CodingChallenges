237bbf25fc0feb448fb4f803e626815c77703a49
tinazarb <tinazarb@gmail.com>
2021-11-16 18:12:55 -0500

    new challenge

---
+++ 8kyu/Fundamentals_MultiplyTwoNumbers.js
@@ -0,0 +1,20 @@
+// Challenge
+Function 3 - multiplying two numbers
+
+Implement a function which multiplies two numbers.
+
+// My Solution:
+function multiply(a,b) {
+  return a * b
+}
+
+
+// Other Solutions:
+let multiply = (a, b) => a * b;
+
+const multiply = (a, b) => a * b;
+
+function multiply(a, b){
+  if(typeof a == 'number' && typeof b == 'number')
+    return a * b;  
+}
