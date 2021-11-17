5b8096e9c0fa4d1a40ddbcdb5fa37f9bf72c3f82
tinazarb <tinazarb@gmail.com>
2021-11-17 18:37:54 -0500

    new challenge

--- JS Algorithms/ReverseString.js
+++ JS Algorithms/ReverseString.js
@@ -12,3 +12,18 @@ function reverseWord(str){
 }
 
 console.log(reverseWord('abc'))
+
+// Other Solutions:
+function reverseStr(str){
+  //split reverse join
+  //return str.split('').reverse().join('')
+  //create empty str. -> loop chars -> add each to str
+  let reverseStr = ""
+  for(let char of str){
+    reverseStr = char + reverseStr
+  }
+  return reverseStr
+}
+
+console.log(reverseStr('bob'), 'bob')
+console.log(reverseStr('john'), 'nhoj')
