dd172b22168faba4bc498ed5f11e9564c8e8830c
Tina Zarbaliev <80288782+tinazarb@users.noreply.github.com>
2021-11-20 11:57:50 -0500

    new challenge

---
+++ 7kyu/Fundamentals_SumofOddNumbers.js
@@ -0,0 +1,43 @@
+// Challenge
+Sum of odd numbers
+
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+
+
+// My Solution:
+function rowSumOddNumbers(n) {
+	return n * n * n
+}
+
+// Other Solutions:
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3);
+}
+
+function rowSumOddNumbers(n) {
+  return n**3
+}
+
+function rowSumOddNumbers(n) {
+  var start = n * n - n + 1;
+  var result = 0;  
+  
+  for(i = 0; i < n; i++)
+  {
+    result =  result + (start + (i*2));
+  }
+    
+   return result;
+}
