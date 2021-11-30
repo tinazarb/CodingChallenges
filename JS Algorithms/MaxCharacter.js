8146c29f4fa12d451b0c27ef4bc05a951aa496a1
tinazarb <tinazarb@gmail.com>
2021-11-29 20:27:06 -0500

    new challenge

--- JS Algorithms/MaxCharacter.js
+++ JS Algorithms/MaxCharacter.js
@@ -28,3 +28,25 @@ function mostOften(str){
   }
   return mostCharacter
 }
+
+// Other Solutions:
+function maxCharacter(str){
+  let charMap = {},
+      count = 0,
+      maxChar = null
+
+  for(const char of str){
+    charMap[char] = charMap[char] + 1 || 1
+  }
+
+  for(const char in charMap){
+    if(charMap[char] > count){
+      count = charMap[char]
+      maxChar = char
+    }
+  }
+
+  return maxChar
+}
+
+console.log(maxCharacter('Hello World!'),'l')
