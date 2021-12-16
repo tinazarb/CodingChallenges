c33d61a70dec9f9e172c049ad3774ab36601d843
tinazarb <tinazarb@gmail.com>
2021-12-16 18:13:43 -0500

    updated solution

--- JS Algorithms/Fibonacci.js
+++ JS Algorithms/Fibonacci.js
@@ -41,3 +41,17 @@ function fib(num){
   if(num<2){return num}
   return fib(num-1) + fib(num-2)
 }
+
+// Fibonacci Memoization
+const memo = {}
+function fib(n) {
+    if(n <= 1){
+        return n
+    }else if( memo[n] ){
+        return memo[n]
+    }else{
+        let result = fib(n - 1) + fib(n - 2)
+        memo[n] = result
+        return result
+    }
+};
