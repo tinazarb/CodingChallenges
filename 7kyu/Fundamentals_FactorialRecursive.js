20c1368d14c5a8637dbfd84d70ba686b8637f289
tinazarb <tinazarb@gmail.com>
2021-12-21 23:52:33 -0500

    new challenge

---
+++ 7kyu/Fundamentals_FactorialRecursive.js
@@ -0,0 +1,38 @@
+// Challenge
+Recursion #1 - Factorial
+
+This is a kata series that you can only solve using recursion.
+
+##1 - Factorial
+
+In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example,
+
+5! = 5 * 4 * 3 * 2 * 1 = 120.
+
+The value of 0! is 1.
+
+#Your task
+
+You have to create the function factorial that receives n and returns n!. You have to use recursion.
+
+console.log(factorial(5))
+console.log(factorial(0))
+
+// My Solution:
+function factorial(n){
+  return n < 2 ? 1 : n * factorial(n-1)
+}
+
+// Other Solutions:
+const factorial = n => {
+  if (n <= 1) return 1;
+  
+  return n * factorial(n - 1);
+};
+
+const factorial = n => n > 1 ? n*factorial(n-1) : 1;
+
+const factorial = n => {
+  if (n == 0) return 1;
+  return n * factorial(n-1);
+};
