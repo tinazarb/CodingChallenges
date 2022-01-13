adc45ebe4d492da0bcb437eb7ec12af0dd09999e
tinazarb <tinazarb@gmail.com>
2022-01-13 17:22:05 -0500

    new challenge

---
+++ 8kyu/Algorithms_CanonsReady.js
@@ -0,0 +1,53 @@
+// Challenge
+Pirates!! Are the Cannons ready!??
+
+Ahoy Matey!
+
+Welcome to the seven seas.
+
+You are the captain of a pirate ship.
+
+You are in battle against the royal navy.
+
+You have cannons at the ready.... or are they?
+
+Your task is to check if the gunners are loaded and ready, if they are: Fire!
+
+If they aren't ready: Shiver me timbers!
+
+Your gunners for each test case are 4 or less.
+
+When you check if they are ready their answers are in a dictionary and will either be: aye or nay
+
+Firing with less than all gunners ready is non-optimum (this is not fire at will, this is fire by the captain's orders or walk the plank, dirty sea-dog!)
+
+If all answers are 'aye' then Fire! if one or more are 'nay' then Shiver me timbers!
+
+var a = {'Mike':'aye','Joe':'aye','Johnson':'aye','Peter':'aye'},
+    b = {'Mike':'aye','Joe':'nay','Johnson':'aye','Peter':'aye'};
+
+console.log(cannonsReady(a), 'Fire!');
+
+console.log(cannonsReady(b), 'Shiver me timbers!')
+
+// My Solution:
+const cannonsReady = (gunners) => {
+for(let i in gunners) {
+ if (gunners[i] === "nay"){
+   return 'Shiver me timbers!'
+ }
+} return 'Fire!'
+}
+
+// Other Solutions:
+const cannonsReady = (gunners) => {
+  return Object.values(gunners).includes('nay')?'Shiver me timbers!':'Fire!';
+}
+
+
+const cannonsReady = (gunners) => {
+  return Object.values(gunners).some(m => m === 'nay') ? 'Shiver me timbers!' : 'Fire!';
+}
+
+const cannonsReady = gunners => 
+  Object.values(gunners).every(val => val === `aye`) ? `Fire!` : `Shiver me timbers!`;
