3ce03fb2abb366f86ae4f90fc4451ff2ec1304ae
tinazarb <tinazarb@gmail.com>
2022-03-17 18:32:11 -0400

    new challenge

---
+++ 8kyu/Fundamentals_VolumeCuboid.js
@@ -0,0 +1,24 @@
+// Challenge
+Volume of a Cuboid
+
+Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+
+
+// My Solution:
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+   return length * width * height
+  }
+}
+
+// Other Solutions:
+class Kata {
+  static getVolumeOfCuboid(l, w, h) {
+    return l * w * h;
+  }
+};
+
+
+const Kata = {
+  getVolumeOfCuboid : (length, width, height) => length * width * height
+}
