694b17fd2d81479dc770450a0d690bc787437c26
tinazarb <tinazarb@gmail.com>
2022-03-29 00:30:48 -0400

    new challenge

--- 7kyu/Fundamentals_DoubleOrNothing.js
+++ 7kyu/Fundamentals_DoubleOrNothing.js
@@ -11,6 +11,35 @@ If I can't afford to pay up, return "I'll pay you back later" instead ;)
 
 
 // My Solution:
-
+function doubleOrNothing(cash, wager, losses){
+  let arr = [wager]
+   for(let i = 0; i < losses-1; i++){  
+     arr.push(arr[i] * 2); 
+   }
+  let count = cash - arr[arr.length-1]
+  if (count >= 0) {return count}
+  else {return "I'll pay you back later"}
+}
 
 // Other Solutions:
+doubleOrNothing=(a,b,c)=>(a=>a<0?"I'll pay you back later":a)(a-b*2**(c-1))
+
+function doubleOrNothing(cash, wager, losses) {
+  for (i=1; i<losses; i++) {
+    wager = wager * 2
+  }
+  if (wager > cash){
+    return 'I\'ll pay you back later'
+  }else {
+    return(cash-wager)
+  }
+}
+
+function doubleOrNothing(cash, wager, losses){
+  cash -= wager * Math.pow(2, losses - 1);
+  if (cash < 0)
+    {
+      return "I'll pay you back later";
+    }
+  return cash;
+}
