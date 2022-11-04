5a31bd48270547e8e5899c397f8960c0cc731133
tinazarb <tinazarb@gmail.com>
2022-11-04 17:41:02 -0400

    Fundamentals_CountDivisorsofaNumber

---
+++ 7kyu/Fundamentals_CountDivisorsofaNumber.js
@@ -0,0 +1,47 @@
+// Challenge:
+Count the divisors of a number
+
+Count the number of divisors of a positive integer n.
+
+Random tests go up to n = 500000.
+Examples (input --> output)
+
+4 --> 3 (1, 2, 4)
+5 --> 2 (1, 5)
+12 --> 6 (1, 2, 3, 4, 6, 12)
+30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+
+Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+
+
+// My Solution:
+function getDivisorsCnt(n){
+let output = 0; 
+    for (let i=1; i<=n; i++) {
+      if (n % i == 0) {
+        output += 1; 
+}
+}
+  return output;
+}
+
+
+// Other Solutions:
+function getDivisorsCnt(n) {
+  for (var d = 0, i = n; i > 0; i--) {
+    if (n % i == 0) d++;
+  }
+  return d;
+}
+
+function getDivisorsCnt(n){
+    var num=0;
+    if(n==1) return 1;
+    if(n%Math.sqrt(n)==0) num++;
+    for(var i=1;i<Math.sqrt(n);i++){
+        if(n%i==0){
+            num+=2;
+        }
+    }
+    return num;
+}
